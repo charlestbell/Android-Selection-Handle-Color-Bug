98d208b00567c18535bf0fe0611ceb58550ae1ec
C.T. Bell <charlestbell@gmail.com>
2024-05-12 14:06:37 -0700

    First Commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          width: 300,
+          margin: 10,
+          textAlign: "center",
+        }}
+        cursorColor={"red"}
+        selectionColor={"red"}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
